f493b1656a43ebb26067f7dd3fb603d5a779ac0d
Ocean01-D <23560085@gm.uit.edu.vn>
2024-12-04 16:57:56 +0700

    change port

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post("/register", (req, res) => {
 });
 
 // Start the server
-const PORT = 4200;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
